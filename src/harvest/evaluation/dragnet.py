61372c89f0fcb7bd0e708688e0252ec2f3d33318
Roger Waldvogel <roger.waldvogel@fhgr.ch>
2020-06-19 15:13:36 +0200

    new: added common start words of posts in comment.

--- src/harvest/evaluation/dragnet.py
+++ src/harvest/evaluation/dragnet.py
@@ -1,3 +1,17 @@
+"""
+Starting words:
+en = ("Hello", "Hi", "Hey", "Have", "Quote from", "I ", "I'", "Is ", "You", "Why", "Where ", "Thank", "Welcome",
+      "The ", "A ", "Here", "Can ", "Could ", "Some ", "For ", "Ok", "Since", "If ", "May ", "Do ", "This ", "When",
+      "That", "My", "Yes", "No", "Are", "How", "There", "Also", "We ", "Mostly", "Due", "What", "Does", "Lovely",
+      "Would", "Please", "Oh ", "Still", "Other ", "Full Name:", "Today", "Just")
+de = ("Hallo", "Hi", "Moin", "So", "Ich", "Wie ", "Was ", "F\u00fcr", "Da ", "Danke", "Willkommen", "Zitat", "Es ",
+      "Eine ", "Du ", "Sch\u00f6nen", "Vielen ", "Mir", "Habe", "Bei", "Guten", "Der", "Liebe", "Super", "Frage", 
+      "Nur", "Das", "Gibt", "Hast", "War", "Sollte", "Wenn", "Dazu", "In ", "Wirklich", "Man ", "Auf", "Ja", "Nein",
+      "Hier", "Jetzt", "Leider", "Hoffe", "Solange", "Von", "Zu ", "Damit")
+fr = ("Bonjour", "Bonsoir", "Je", "les ", "Oui", "C'est ", "Merci", "Bienvenue", "Pour ", "La ", "Le ")
+sp = ("Hola", "Perdon", "Me ", "Quer\u00eda ", "Juanjo", "Buenas ")
+"""
+
 from dragnet import extract_content_and_comments
 from harvest.date_search import search_dates
 
