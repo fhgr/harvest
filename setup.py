db8891092a219f9534d5d9379751211ae163ae77
roger <climberwoodi@gmx.ch>
2020-12-04 16:33:06 +0100

    chg: correct read of README.md. Using name harvest-webforum

--- setup.py
+++ setup.py
@@ -10,15 +10,17 @@ sys.path.insert(0, path.join(here, 'src'))
 
 from harvest import (__version__, __author__, __author_email__, __license__)
 
-with open(path.join(here, 'README.md')) as f:  # , encoding='utf-8'
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     # Metadata
-    name="harvest",
+    name="harvest-webforum",
     version=__version__,
     description='A toolkit for extracting posts and post metadata from web forums',
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author=__author__,
     author_email=__author_email__,
     python_requires='>=3.5',
